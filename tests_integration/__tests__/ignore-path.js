8a0fdeff7978cdb140ea7d7d6a47e87be008d6d9
Brian Malehorn <bmalehorn@gmail.com>
2019-06-07 22:31:49 -0700

    moar tests

--- tests_integration/__tests__/ignore-path.js
+++ tests_integration/__tests__/ignore-path.js
@@ -3,7 +3,7 @@
 const runPrettier = require("../runPrettier");
 
 describe("ignore path", () => {
-  runPrettier("cli/ignore-path", [
+  runPrettier("cli/ignore-path/ignore-regular", [
     "**/*.js",
     "--ignore-path",
     ".gitignore",
@@ -14,19 +14,22 @@ describe("ignore path", () => {
 });
 
 describe("support .prettierignore", () => {
-  runPrettier("cli/ignore-path", ["**/*.js", "-l"]).test({
+  runPrettier("cli/ignore-path/ignore-regular", ["**/*.js", "-l"]).test({
     status: 1
   });
 });
 
 describe("ignore file when using --debug-check", () => {
-  runPrettier("cli/ignore-path", ["**/*.js", "--debug-check"]).test({
+  runPrettier("cli/ignore-path/ignore-regular", [
+    "**/*.js",
+    "--debug-check"
+  ]).test({
     status: 0
   });
 });
 
 describe("outputs files as-is if no --write", () => {
-  runPrettier("cli/ignore-path", ["regular-module.js"], {
+  runPrettier("cli/ignore-path/ignore-regular", ["regular-module.js"], {
     ignoreLineEndings: true
   }).test({
     status: 0
